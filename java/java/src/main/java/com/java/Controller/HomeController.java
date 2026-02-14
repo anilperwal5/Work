506398f65c6e8d11fa2a9353a108d26ecde9e472
Anil Perwal <anilperwal5@gmail.com>
2026-02-14 21:30:07 +0530

    BlackBerry project

---
+++ java/java/src/main/java/com/java/Controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.java.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/signup")
+    public String SignUp(){
+
+        return "signup";
+    }
+
+
+    @PostMapping("/register")
+    public void register(){
+
+    }
+
+}
---
+++ java/java/src/main/java/com/java/Controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.java.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "This is index page";
+    }
+
+}
